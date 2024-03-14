1167541966cb2467830f94096323736e0fe1b305
anasenicenkova <senichenkova02@inbox.ru>
2024-03-15 00:34:13 +0300

    Edited parse json func

--- api/utils.py
+++ api/utils.py
@@ -1,5 +1,3 @@
-import json
-
 from fastapi.security import HTTPBearer
 
 security = HTTPBearer(scheme_name='Authorization')
@@ -10,27 +8,23 @@ def get_dict_by_key(list_of_dicts, key_name, key_value):
     return next((dict for dict in list_of_dicts if dict[key_name] == key_value), None)
 
 
-def create_readable_text(json_file_path):
+def create_readable_text(data):
     """Полученный json от API перемалывает в читабельный вид"""
-    """Пример использования result = create_readable_text("temp.json")"""
+    """Пример использования result = create_readable_text(data)"""
 
-    with open(json_file_path, 'r', encoding='utf-8') as file:
-        data = json.load(file)
-    result_list=[]
-    
+    result_list = []
 
     for item in data['data']:
         result = {}
-        result["Дата заезда"]= {item[0]}
-        result["Длительность в ночах"]  ={item[3]}
-        result["Регион проживания"] ={item[5][0]}
-        result["Отель"] ={item[6][1]}
-        result["Пансион"] ={item[7][1]}
-        result["Тип номера"]= {item[8][1]}
-        result["Цена"] = float(item[10]['total'])*90
-        result["Доступные места в отеле"]= item[11]
+        result["Дата заезда"] = item[0]
+        result["Длительность в ночах"] = item[3]
+        result["Регион проживания"] = item[5][0]
+        result["Отель"] = item[6][1]
+        result["Пансион"] = item[7][1]
+        result["Тип номера"] = item[8][1]
+        result["Цена"] = float(item[10]['total']) * 90
+        result["Доступные места в отеле"] = item[11][0][0]
         result_list.append(result)
 
     return result_list
-
-print(create_readable_text('test.json'))
+# print(create_readable_text('test.json'))
