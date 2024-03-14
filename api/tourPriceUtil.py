e20da21f75f31b637284c9545cf9eefb4a36bffd
RichZcukini <sevokzcukini@gmail.com>
2024-03-15 01:05:25 +0300

    Сделал скелет класса для обработки словарей со сценами

---
+++ api/tourPriceUtil.py
@@ -0,0 +1,12 @@
+class Tour:
+    date = "2050-01-01"
+    nightCount = -1
+    region = "Test Region"
+    hotel = "Test Hotel"
+    feeding = "Test food"
+    roomType = "Test Room Type"
+    price = 1
+    availability = "Test link"
+
+    def __init__(self):
+        pass
