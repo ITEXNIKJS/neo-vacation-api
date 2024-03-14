177fc87fd304b0f8d33a0407a37d153404a8eb1c
RichZcukini <sevokzcukini@gmail.com>
2024-03-14 20:19:10 +0300

    Исправил формулу и удалил чтение данных из файла

--- api/hr-plug.py
+++ api/hr-plug.py
@@ -23,17 +23,8 @@ class Employee:
         #среднегодовой заработок * кол-во дней в отпуске / среднее кол-во дней в месяце
         coef = self.vacationDaysAvailable
         if (self.vacationDaysAvailable > vacationDays):
-            coef = self.vacationDaysAvailable - vacationDays
+            coef = vacationDays - self.vacationDaysAvailable
         vacationPay = self.avgSalary * coef / days_in_month
         return vacationPay
 
-#Чтение сотрудников из файла TODO
-def getEmployees():
-    hr = []
-    with open("hr.txt", 'r') as file:
-        for line in file:
-            id, name, vacationDaysAvailable, lastVacation, currentSalary, avgSalary = line.strip().split(split_char)
-            employee = Employee(id, name, int(vacationDaysAvailable), lastVacation, int(currentSalary), int(avgSalary))
-            hr.append(employee)
-    file.close()
-    return hr
+
