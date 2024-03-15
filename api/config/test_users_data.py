863af592ef6e78fcff59a4cafe14462b4a2c2b74
anasenicenkova <senichenkova02@inbox.ru>
2024-03-15 16:46:07 +0300

    debugged data filter

--- api/config/test_users_data.py
+++ api/config/test_users_data.py
@@ -1,8 +1,16 @@
-test_data = {
+test_data = [{
     'id': '',
     'name': 'Алексей Алексеев',
     'vacationDaysAvailable': 5,
     'lastVacation': '12.02.2024',
     'currentSalary': 100000,
     'avgSalary': 100000
-}
+},
+{
+    'id': '',
+    'name': 'Алексей Алексеев',
+    'vacationDaysAvailable': 5,
+    'lastVacation': '12.02.2024',
+    'currentSalary': 100000,
+    'avgSalary': 100000
+}]
