4ecb7eb78577ddf58f8a615c0a21d2a83168e110
RichZcukini <sevokzcukini@gmail.com>
2024-03-15 14:34:47 +0300

    Функция подсчёта итоговых дней отпуска (в момент начала отпуска) Так же, изменил входные параметры основной функции на подсчёт отпускных, она требует ещё и дату заезда.

--- api/hr_plug.py
+++ api/hr_plug.py
@@ -1,7 +1,21 @@
-# Среднее кол-во дней в месяце, даёт лишь примерный результат
+from datetime import datetime, timedelta
 
 split_char = '|'
-days_in_month = 29.3
+days_in_month = 29.3 # Среднее кол-во дней в месяце, даёт лишь примерный результат
+
+
+Holidays = [
+    "01.01.2024",    "02.01.2024",
+    "03.01.2024",    "04.01.2024",
+    "05.01.2024",    "06.01.2024",
+    "08.01.2024",    "07.01.2024",
+    "23.02.2024",    "08.03.2024",
+    "01.05.2024",
+    "09.05.2024",    "12.06.2024",
+    "04.11.2024"
+]
+
+
 class Employee:
     id = -1
     name = "Test Data"
@@ -19,18 +33,38 @@ class Employee:
         self.avgSalary = avgSalary
 
     #Расчитать отпускные и дополнительные убытки сотрудника, если отпускных мало
-    def vacation_pay(self, vacationDays):
+    def vacation_pay(self, departureDay, tourDurationInDays):
+        #TODO: учёт праздничных дней!!!
         #среднегодовой заработок * кол-во дней в отпуске / среднее кол-во дней в месяце
-        coef = vacationDays #сколько дней оплачивается
+        coef = tourDurationInDays #сколько дней оплачивается
         employeeLosses = 0 #Убытки сотрудника, если ему не хватило дней
+        totalPaidDays = self.count_available_days(departureDay, tourDurationInDays)
 
         #если дней отпуска не хватило
-        if (self.vacationDaysAvailable < vacationDays):
-            coef = vacationDays - self.vacationDaysAvailable
-            employeeLosses = self.avgSalary / days_in_month * (vacationDays - self.vacationDaysAvailable)
-            employeeLosses += self.avgSalary / days_in_month * (self.vacationDaysAvailable - coef)
+        if (totalPaidDays < tourDurationInDays):
+            coef = tourDurationInDays - totalPaidDays
+            employeeLosses = self.avgSalary / days_in_month * (tourDurationInDays - totalPaidDays)
+            employeeLosses += self.avgSalary / days_in_month * (totalPaidDays - coef)
 
         vacationPay = self.avgSalary / days_in_month * coef
         return vacationPay, employeeLosses
 
+    #Расчитать, сколько дней отпуска будет накоплено к дате отправления в тур
+    def count_available_days(self, departureDate, tourDurationInDays):
+        startDate = datetime.strptime(self.lastVacation, "%d.%m.%Y").date()
+        endDate = datetime.strptime(departureDate, "%d.%m.%Y").date()
+        totalPaidDays = self.vacationDaysAvailable
 
+        #Прибавление новых отпускных, если прошёл год
+        if (endDate - startDate >= timedelta(365)):
+            totalPaidDays += 28
+
+        #Смотрим, будут ли праздники во время тура
+        checkDate = endDate
+        for i in range(tourDurationInDays):
+            date_str = checkDate.strftime("%d.%m.%Y")
+            if (date_str in Holidays):
+                totalPaidDays += 1
+            checkDate += timedelta(days=1)
+        print("totalPaidDays: ", totalPaidDays)
+        return totalPaidDays
--- api/tourPriceUtil.py
+++ api/tourPriceUtil.py
@@ -1,18 +1,19 @@
 from hr_plug import *
 
+#Вычесть убытки из цены тура (или туров)
 def count_losses(employee, tours):
     if not tours:
         return -1
     if type(tours) is list:
         for tour in tours:
-            payment, losses = employee.vacation_pay(list(tour["Длительность в ночах"])[0])
+            payment, losses = employee.vacation_pay(list(tour["Дата заезда"])[0], list(tour["Длительность в ночах"])[0])
             tour["Цена"] -= losses
     else:
         payment, losses = employee.vacation_pay(list(tours["Длительность в ночах"])[0])
         tours["Цена"] -= losses
     return tours
 
-#test = [{'Дата заезда': {'19.03.2024'}, 'Длительность в ночах': {10}, 'Регион проживания': {'Белек'}, 'Отель': {'PRENSES SEALINE BEACH HOTEL 4 *'}, 'Пансион': {'Только завтраки'}, 'Тип номера': {'Economy Room without Balcony'}, 'Цена': 102922.11, 'Доступные места в отеле': [['https://online.tez-tour.com/armmanager/workplace/section/new-order?depCity=345&arrivalCity=345&hotStType=2&locale=ru&priceOfferId=21969333&cResId=277490456155&cFlyIds=227480733&ftt=3635&ltt=3635&ftv=&ltv=&sk=1&promo=1&rar=487545&rdr=487545', '']]}]
-#employee = Employee(1, "test", 2, "10.03.2020", 10000, 10000)
-#print(count_losses(employee, test))
+test = [{'Дата заезда': {'19.03.2024'}, 'Длительность в ночах': {10}, 'Регион проживания': {'Белек'}, 'Отель': {'PRENSES SEALINE BEACH HOTEL 4 *'}, 'Пансион': {'Только завтраки'}, 'Тип номера': {'Economy Room without Balcony'}, 'Цена': 102922.11, 'Доступные места в отеле': [['https://online.tez-tour.com/armmanager/workplace/section/new-order?depCity=345&arrivalCity=345&hotStType=2&locale=ru&priceOfferId=21969333&cResId=277490456155&cFlyIds=227480733&ftt=3635&ltt=3635&ftv=&ltv=&sk=1&promo=1&rar=487545&rdr=487545', '']]}]
+employee = Employee(1, "test", 2, "10.03.2020", 10000, 10000)
+print(count_losses(employee, test))
 
