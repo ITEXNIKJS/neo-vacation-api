d314baa0e071d232a578e4f65648ddd99ab753bd
anasenicenkova <senichenkova02@inbox.ru>
2024-03-16 00:37:05 +0300

    added stats funcs

--- api/utils.py
+++ api/utils.py
@@ -1,6 +1,7 @@
 import math
 from datetime import datetime
 
+import pandas as pd
 import requests
 from dateutil.relativedelta import relativedelta
 from fastapi.security import HTTPBearer
@@ -100,17 +101,215 @@ def calc_new_date(current_vacation_days, required_vacation_days):
         return today.date()
 
 
+def get_pairs_data_price(df):
+    date_in_series = pd.Series(df["date_in"])
+    price_series = pd.Series(df["price"])
+    pairs = list(zip(date_in_series, price_series))
+    return pairs
+
+
+def get_pairs_data_price_loss(df):
+    date_in_series = pd.Series(df["date_in"])
+    price_series = pd.Series(df["price_with_loss"])
+    pairs = list(zip(date_in_series, price_series))
+    return pairs
+
+
+def get_pairs_area_price(df):
+    date_in_series = pd.Series(df["area"])
+    price_series = pd.Series(df["price"])
+    pairs = list(zip(date_in_series, price_series))
+    return pairs
+
+
+def get_pairs_area_price_loss(df):
+    date_in_series = pd.Series(df["area"])
+    price_series = pd.Series(df["price_with_loss"])
+    pairs = list(zip(date_in_series, price_series))
+    return pairs
+
 if __name__ == "__main__":
-    current_vacation_days = 9
-    required_vacation_days = 8
+    data = [
+        {
+            "date_in": "2024-03-12",
+            "amount_of_nights": 3,
+            "area": "Расссиииияяяя",
+            "hotel": "Примерный",
+            "room_type": "Стандартный",
+            "price": 100.0,
+            "availible_rooms": "Да",
+            "price_with_loss": 90.0,
+            "category": "Эконом",
+            "buy_date": "2024-03-09",
+        },
+        {
+            "date_in": "2024-03-22",
+            "amount_of_nights": 3,
+            "area": "Москва",
+            "hotel": "Примерный",
+            "room_type": "Стандартный",
+            "price": 200.0,
+            "availible_rooms": "Да",
+            "price_with_loss": 90.0,
+            "category": "Эконом",
+            "buy_date": "2024-03-05",
+        },
+        {
+            "date_in": "2024-03-22",
+            "amount_of_nights": 3,
+            "area": "Москва",
+            "hotel": "Примерный",
+            "room_type": "Стандартный",
+            "price": 200.0,
+            "availible_rooms": "Да",
+            "price_with_loss": 90.0,
+            "category": "Эконом",
+            "buy_date": "2024-03-05",
+        },
+        {
+            "date_in": "2024-03-22",
+            "amount_of_nights": 3,
+            "area": "Москва",
+            "hotel": "Примерный",
+            "room_type": "Стандартный",
+            "price": 200.0,
+            "availible_rooms": "Да",
+            "price_with_loss": 90.0,
+            "category": "Эконом",
+            "buy_date": "2024-03-05",
+        },
+        {
+            "date_in": "2024-03-22",
+            "amount_of_nights": 3,
+            "area": "Москва",
+            "hotel": "Примерный",
+            "room_type": "Стандартный",
+            "price": 200.0,
+            "availible_rooms": "Да",
+            "price_with_loss": 90.0,
+            "category": "Эконом",
+            "buy_date": "2024-03-05",
+        },
+        {
+            "date_in": "2024-03-22",
+            "amount_of_nights": 3,
+            "area": "Москва",
+            "hotel": "Примерный",
+            "room_type": "Стандартный",
+            "price": 200.0,
+            "availible_rooms": "Да",
+            "price_with_loss": 90.0,
+            "category": "Эконом",
+            "buy_date": "2024-03-05",
+        },
+        {
+            "date_in": "2024-03-22",
+            "amount_of_nights": 3,
+            "area": "Москва",
+            "hotel": "Примерный",
+            "room_type": "Стандартный",
+            "price": 200.0,
+            "availible_rooms": "Да",
+            "price_with_loss": 90.0,
+            "category": "Эконом",
+            "buy_date": "2024-03-05",
+        },
+        {
+            "date_in": "2024-03-13",
+            "amount_of_nights": 3,
+            "area": "Москва",
+            "hotel": "Примерный",
+            "room_type": "Стандартный",
+            "price": 350.0,
+            "availible_rooms": "Да",
+            "price_with_loss": 90.0,
+            "category": "Эконом",
+            "buy_date": "2024-03-05",
+        },
+        {
+            "date_in": "2024-03-18",
+            "amount_of_nights": 3,
+            "area": "Москва",
+            "hotel": "Примерный",
+            "room_type": "Стандартный",
+            "price": 400.0,
+            "availible_rooms": "Да",
+            "price_with_loss": 90.0,
+            "category": "Эконом",
+            "buy_date": "2024-03-05",
+        },
+        {
+            "date_in": "2024-03-19",
+            "amount_of_nights": 3,
+            "area": "Москва",
+            "hotel": "Примерный",
+            "room_type": "Стандартный",
+            "price": 150.0,
+            "availible_rooms": "Да",
+            "price_with_loss": 90.0,
+            "category": "Эконом",
+            "buy_date": "2024-03-05",
+        },
+        {
+            "date_in": "2024-03-30",
+            "amount_of_nights": 3,
+            "area": "Москва",
+            "hotel": "Примерный",
+            "room_type": "Стандартный",
+            "price": 2000.0,
+            "availible_rooms": "Да",
+            "price_with_loss": 90.0,
+            "category": "Эконом",
+            "buy_date": "2024-03-05",
+        },
+        {
+            "date_in": "2024-03-20",
+            "amount_of_nights": 3,
+            "area": "Москва",
+            "hotel": "Примерный",
+            "room_type": "Стандартный",
+            "price": 210.0,
+            "availible_rooms": "Да",
+            "price_with_loss": 90.0,
+            "category": "Эконом",
+            "buy_date": "2024-03-05",
+        },
+        {
+            "date_in": "2024-03-21",
+            "amount_of_nights": 3,
+            "area": "Москва",
+            "hotel": "Примерный",
+            "room_type": "Стандартный",
+            "price": 240.0,
+            "availible_rooms": "Да",
+            "price_with_loss": 90.0,
+            "category": "Эконом",
+            "buy_date": "2024-03-05",
+        },
+        {
+            "date_in": "2024-03-22",
+            "amount_of_nights": 3,
+            "area": "Москва",
+            "hotel": "Примерный",
+            "room_type": "Стандартный",
+            "price": 230.0,
+            "availible_rooms": "Да",
+            "price_with_loss": 90.0,
+            "category": "Эконом",
+            "buy_date": "2024-03-05",
+        },
+    ]
 
-    print(calc_new_date(current_vacation_days, required_vacation_days))
 
-if __name__ == '__main__':
-    # test = [{'Дата заезда': '19.03.2024', 'Длительность в ночах': 10, 'Регион проживания': {'Белек'},
-    #          'Отель': {'PRENSES SEALINE BEACH HOTEL 4 *'}, 'Пансион': 'Только завтраки',
-    #          'Тип номера': {'Economy Room without Balcony'}, 'Цена': 102922.11, 'Доступные места в отеле': [[
-    #                                                                                                             'https://online.tez-tour.com/armmanager/workplace/section/new-order?depCity=345&arrivalCity=345&hotStType=2&locale=ru&priceOfferId=21969333&cResId=277490456155&cFlyIds=227480733&ftt=3635&ltt=3635&ftv=&ltv=&sk=1&promo=1&rar=487545&rdr=487545',
-    #                                                                                                             '']]}]
-    # employee = Employee(1, "test", 2, "10.03.2020", 10000, 10000)
-    print(get_value_USD())
+    def get_df(data):
+        for entry in data:
+            entry["date_in"] = pd.to_datetime(entry["date_in"])
+            entry["buy_date"] = pd.to_datetime(entry["buy_date"])
+
+        return pd.DataFrame(data)
+
+    df = get_df(data)
+    print(get_pairs_area_price_loss(df))
+
+
+
